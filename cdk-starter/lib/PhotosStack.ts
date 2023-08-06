6fbe992fcf1708094c1b54d31013faf793341425
Asada678 <asada.tomoya0923@gmail.com>
2023-08-06 20:15:01 +0900

    create suffix

--- cdk-starter/lib/PhotosStack.ts
+++ cdk-starter/lib/PhotosStack.ts
@@ -1,17 +1,22 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Fn, Stack, StackProps } from "aws-cdk-lib";
 import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class PhotosStack extends Stack {
+  private stackSuffix: string;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const myBucket = new Bucket(this, "PhotosBucket", {
-      bucketName: "photosbucket-asadatomoya01",
-    });
+    this.initializeSuffix();
 
-    (myBucket.node.defaultChild as CfnBucket).overrideLogicalId('PhotosBucket923asadatomoya')
-    // create a new resource
-    // delete the old one
+    new Bucket(this, "PhotosBucket", {
+      bucketName: `photos-bucket-${this.stackSuffix}`,
+    });
+  }
+
+  private initializeSuffix() {
+    const shortStackId = Fn.select(2, Fn.split("/", this.stackId));
+    this.stackSuffix = Fn.select(4, Fn.split("-", shortStackId));
   }
 }
