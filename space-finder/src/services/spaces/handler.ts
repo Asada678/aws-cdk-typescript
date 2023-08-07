6ff09eb44c865123f82b09db0a5b29ee235609ff
Asada678 <asada.tomoya0923@gmail.com>
2023-08-07 16:52:34 +0900

    DynamoDB PutItem

--- space-finder/src/services/spaces/handler.ts
+++ space-finder/src/services/spaces/handler.ts
@@ -1,23 +1,38 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
+import { postSpaces } from "./PostSpaces";
+
+// outside of handler, the resources can be reused
+const ddbClient = new DynamoDBClient({});
 
 async function handler(
   event: APIGatewayProxyEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> {
   let message: string;
-  switch (event.httpMethod) {
-    case "GET":
-      message = "Hello from GET!";
-      break;
-    case "POST":
-      message = "Hello from POST!";
-      break;
-    default:
-      break;
+
+  try {
+    switch (event.httpMethod) {
+      case "GET":
+        message = "Hello from GET!";
+        break;
+      case "POST":
+        const response = postSpaces(event, ddbClient);
+        return response;
+        break;
+      default:
+        break;
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    };
   }
   const response: APIGatewayProxyResult = {
     statusCode: 200,
