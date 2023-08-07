7c127f5aea13222c873de978a5196013a8b2af11
Asada678 <asada.tomoya0923@gmail.com>
2023-08-07 18:22:17 +0900

    validation

--- space-finder/test/launcher.ts
+++ space-finder/test/launcher.ts
@@ -5,13 +5,15 @@ process.env.TABLE_NAME = "SpaceTable-0e779e6ed181";
 
 handler(
   {
-    httpMethod: "DELETE",
-    queryStringParameters: {
-      id: "a3c8b374-76b1-43b0-83a1-d4c31090dc29",
-    },
-    // body: JSON.stringify({
-    //   location: "Japan updated",
-    // }),
+    httpMethod: "POST",
+    // queryStringParameters: {
+    //   id: "a3c8b374-76b1-43b0-83a1-d4c31090dc29",
+    // },
+    body: JSON.stringify({
+      location: "Japan updated",
+    }),
   } as any,
   {} as any
-);
+).then((result) => {
+  console.log("result: ", result);
+});
