c98ea187ac9cad26ff0996dcf628e0dfdeb332e9
Asada678 <asada.tomoya0923@gmail.com>
2023-08-08 19:08:19 +0900

    Securing API with Cognito

--- space-finder/test/auth.test.ts
+++ space-finder/test/auth.test.ts
@@ -4,6 +4,7 @@ async function testAuth() {
   const service = new AuthService();
   const loginResult = await service.login("zaq", "zaq1ZAQ!");
   console.log("loginResult: ", loginResult);
+  console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());
 }
 
 testAuth();
