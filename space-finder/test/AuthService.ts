4cee8678cbcafc85118747a5856308413f4f5f13
Asada678 <asada.tomoya0923@gmail.com>
2023-08-09 14:40:24 +0900

    generateTemporaryCredentials

--- space-finder/test/AuthService.ts
+++ space-finder/test/AuthService.ts
@@ -1,5 +1,7 @@
 import { type CognitoUser } from "@aws-amplify/auth";
 import { Amplify, Auth } from "aws-amplify";
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
 
 const awsRegion = "ap-northeast-1";
 
@@ -8,6 +10,7 @@ Amplify.configure({
     region: awsRegion,
     userPoolId: "ap-northeast-1_WOb5qNeC7",
     userPoolWebClientId: "46etr5vm7rhl1tstan2lrkm9r1",
+    identityPoolId: "ap-northeast-1:4c6b4a06-9a32-452b-a3ce-a8f4a3240da5",
     authenticationFlowType: "USER_PASSWORD_AUTH",
   },
 });
@@ -16,5 +19,20 @@ export class AuthService {
   public async login(username: string, password: string) {
     const result = (await Auth.signIn(username, password)) as CognitoUser;
     return result;
+  }
+
+  public async generateTemporaryCredentials(user: CognitoUser) {
+    const jwtToken = user.getSignInUserSession().getIdToken().getJwtToken();
+    const cognitoIdentityPool = `cognito-idp.${awsRegion}.amazonaws.com/ap-northeast-1_WOb5qNeC7`;
+    const cognitoIdentity = new CognitoIdentityClient({
+      credentials: fromCognitoIdentityPool({
+        identityPoolId: "ap-northeast-1:4c6b4a06-9a32-452b-a3ce-a8f4a3240da5",
+        logins: {
+          [cognitoIdentityPool]: jwtToken,
+        },
+      }),
+    });
+    const credentials = await cognitoIdentity.config.credentials();
+    return credentials;
   }
 }
--- space-finder/test/auth.test.ts
+++ space-finder/test/auth.test.ts
@@ -3,8 +3,11 @@ import { AuthService } from "./AuthService";
 async function testAuth() {
   const service = new AuthService();
   const loginResult = await service.login("zaq", "zaq1ZAQ!");
-  console.log("loginResult: ", loginResult);
-  console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());
+  // console.log("loginResult: ", loginResult);
+  // console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());
+
+  const credentials = await service.generateTemporaryCredentials(loginResult);
+  console.log("credentials: ", credentials);
 }
 
 testAuth();
